d890882e75e2c78b15442ebc4317fcb84471dbdb
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2025-08-18 18:45:13 -0300

    Subida inicial de la Guía 1 de HDL (Verilog)
    - Se agregó la estructura básica del repositorio.
    - Se implementaron los primeros ejercicios de la materia Arquitectura de Computadoras I.
    - Incluye módulos en Verilog y bancos de prueba para simulación con Icarus Verilog + GTKWave.
    
    Autor: Marcos Quinteros

--- ejercicio 1/ejercicio1.v
+++
@@ -1,6 +0,0 @@
-//Diseñe un módulo en Verilog que implemente un sumador completo de un bit con entradas a,b y
-//cin(carry in), y salidas sum y cout (carry out). Describir y simular con Icarus Verilog 
-module ejercicio1 ( input a , input b , input cin , output sum , output co ) ;
-assign sum = a ^ b ^ cin ;
-assign co = ( a & b ) | cin & ( a ^ b ) ;
-endmodule
