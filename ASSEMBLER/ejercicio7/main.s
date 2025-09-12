d807ce1f8edd4132232db115cc78ee3eb0ef5a05
marcosQuinteros <marcosquinteros2003@gmail.com>
2025-09-12 19:24:49 -0300

    Ejercicio 7

--- ASSEMBLER/ejercicio7/main.s
+++ ASSEMBLER/ejercicio7/main.s
@@ -3,13 +3,40 @@
 //direcciones base de los arreglos A y B están en los registros X6 y X7, respectivamente.
 //1 B [8] = A [i - j
 
-	.data
-	i: .dword 5
-	j: .dword 10
-    	
+	    .data
+A:   .dword 0,1,2,3,4,5,6,7,8,9   // Arreglo A
+B:   .dword 0,0,0,0,0,0,0,0,0,0   // Arreglo B
+i:   .dword 8
+j:   .dword 3
 
-	.text
+    .text
+    .global _start
+_start:
 
+    // Cargar direcciones de i y j
+    LDR X3, =i
+    LDR X4, =j
+    LDR X6, =A          // Base de A
+    LDR X7, =B          // Base de B
 
-end:
-infloop: B infloop
+    // Obtener los valores de i y j (porque LDR X3,=i trae la dirección)
+    LDUR X3, [X3]        // X3 = valor de i
+    LDUR X4, [X4]        // X4 = valor de j
+
+    // Calcular i - j
+    SUB X8, X3, X4      // X8 = i - j
+
+    // Calcular desplazamiento en bytes para A[i-j]
+    LSL X9, X8, #3      // X9 = (i-j) * 8
+
+    // Cargar A[i-j]
+    LDR X10, [X6, X9]   // X10 = A[i-j]
+
+    // Calcular dirección de B[8] (8*8 = 64 bytes)
+    MOVZ X11, #64       // X11 = 64
+
+    // Guardar en B[8]
+    STR X10, [X7, X11]  // B[8] = A[i-j]
+
+infloop: 
+    B infloop           // loop infinito (para debug)
